f0a7a9d5c80381f1f1f86f0e279b49d8f2aa3427
Lucas Borges <lucas-borges2000@hotmail.com>
2025-05-31 09:09:02 -0300

    corrigido o navibar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,38 +1,37 @@
-<nav class="navbar navbar-expand-lg bg-white py-3">
+<nav class="navbar navbar-expand-lg bg-white py-3 shadow-sm">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="#">
-            <img src="{{ asset('images/Icone.png') }}" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
-            Lalasia
+      <a class="navbar-brand d-flex align-items-center fw-bold" href="#">
+        <img src="{{ asset('images/Icone.png') }}" alt="Logo" width="30" height="30" class="me-2">
+        Lalasia
+      </a>
+  
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item mx-2">
+            <a class="nav-link text-dark fw-semibold" href="#">Product</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link text-dark fw-semibold" href="#">Services</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link text-dark fw-semibold" href="#">Article</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link text-dark fw-semibold" href="#">About Us</a>
+          </li>
+        </ul>
+      </div>
+        <a href="#" class="text-dark fs-5 me-3">
+          <i class="bi bi-bag"></i>
         </a>
-        
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Article</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About Us</a>
-                </li>
-            </ul>
-            
-            <div class="d-flex">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search property">
-                    <button class="btn btn-outline-secondary" type="button">
-                        <i class="bi bi-search"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
+        <a href="#" class="text-dark fs-5">
+          <i class="bi bi-person"></i>
+        </a>
+      </div>
     </div>
-</nav>
+  </nav>
+
