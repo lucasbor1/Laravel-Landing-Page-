c94b7cc99202d2dd7fddb2b639a87c8680b6f1c3
Lucas Borges <lucas-borges2000@hotmail.com>
2025-06-01 18:54:11 -0300

    feat: bouttom back to top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
     <title>Lalasia - Discover Furniture</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+<button id="backToTop" class="back-to-top" aria-label="Voltar ao topo">
+    <i class="bi bi-arrow-up"></i>
+</button>
+
 <body>
     @include('components.navbar')
     
